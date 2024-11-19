406a37de68f2cc5fbb6271159f658eabc49e4fda
vaides4 <vai22057@uvg.edu.gt>
2024-11-18 21:10:07 -0600

    Create fonth.h

---
+++ fonth.h
@@ -0,0 +1,18 @@
+#ifndef INC_FONT_H_
+#define INC_FONT_H_
+
+#include <stdint.h>
+
+#define fontXSizeSmal 8
+#define fontYSizeSmal 12
+#define fontXSizeBig 16
+#define fontYSizeBig 16
+#define fontXSizeNum 32
+#define fontYSizeNum 50
+
+#define fontdatatype const uint8_t
+#define fontdatatype16 const uint16_t
+
+
+
+#endif
